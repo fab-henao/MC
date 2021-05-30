72ab7a3bf389e48dabae36b34eba17a349155298
fab-henao <fabianfeik474@gmail.com>
2021-05-29 22:28:45 -0500

    0.0.1

--- setup.py
+++ setup.py
@@ -6,5 +6,8 @@ setup(
     author='Fabian Henao',
     description='Es un paquete que te permitira obtener datos de un usuario de Minecraft y de los Servicios de Mojan y servidores de Minecraft',
     install_requires='required_modules',
-    author_mail='fabianfeik474@gmail.com'
+    author_mail='fabianfeik474@gmail.com',
+    url='https://github.com/fab-henao/MC.git',
+    keywords=["mojang", "minecraft", "api", "mojang api", "minecraft api"],
+    python_requires='>=3.6'
 )
