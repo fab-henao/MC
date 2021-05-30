d66d7941af32c7dc4254f06e9223bd3bcf282914
fab-henao <fabianfeik474@gmail.com>
2021-05-29 22:20:45 -0500

    0.0.1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import  setup, find_packages
+
+setup(
+    name='MC',
+    version='0.0.1',
+    author='Fabian Henao',
+    description='Es un paquete que te permitira obtener datos de un usuario de Minecraft y de los Servicios de Mojan y servidores de Minecraft',
+    install_requires='required_modules',
+    author_mail='fabianfeik474@gmail.com'
+)
